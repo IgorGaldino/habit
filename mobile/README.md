3f688c90a2ce86104f4d15ce729d24cfb93e3f5d
Igor Galdino <igormgaldino@gmail.com>
2023-01-22 12:11:59 -0300

    Mobile finished

---
+++ mobile/README.md
@@ -0,0 +1,2 @@
+# habit-mobile
+Aplicação mobile desenvolvida no NLW setup
---
+++ mobile/src/components/HabitsEmpty.tsx
@@ -0,0 +1,19 @@
+import { Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export function HabitsEmpty() {
+  const { navigate } = useNavigation();
+
+  return (
+    <Text className="text-zinc-400 text-base">
+      Você ainda não estão monitorando nenhum hábito {" "}
+
+      <Text
+        className="text-violet-400 text-base underline active:text-violet-500"
+        onPress={() => navigate("new")}
+      >
+        comece cadastrando um.
+      </Text>
+    </Text>
+  );
+}
